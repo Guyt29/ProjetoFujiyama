fdf6b2b852b5b4f1bdb51e17de5d63b0d726bbcb
Gustavo <gustavotoyotadev@gmail.com>
2025-10-12 22:02:45 -0300

    Pagina inicial

---
+++ projetoFuji/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetoFuji.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
